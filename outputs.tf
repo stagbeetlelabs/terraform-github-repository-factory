b857966750233300df688b96472f4bb3644c30e1
William Heyland <william.heyland@newfold.com>
2026-02-27 19:01:02 +0000

    Improve outputs.

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "repo_owner" {
-  value = var.repo_owner
+  value = split("/", github_branch_protection.branch_protection.repository_id)[0]
 }
 
 output "repo_name" {
-  value = var.repo_name
+  value = split("/", github_branch_protection.branch_protection.repository_id)[1]
 }
