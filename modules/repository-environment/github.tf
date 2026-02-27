d3d324cc5f2bda012707e1ae684ceeab050224b5
William Heyland <william.heyland@newfold.com>
2026-02-27 19:33:55 +0000

    Improve outputs.

--- modules/repository-environment/github.tf
+++ modules/repository-environment/github.tf
@@ -3,7 +3,7 @@
  */
 resource "github_repository_environment" "environment" {
   environment = var.env_code
-  repository  = "${var.repo_owner}/${var.repo_name}"
+  repository  = var.repo_name
 
   deployment_branch_policy {
     protected_branches     = true
@@ -16,7 +16,7 @@ resource "github_repository_environment" "environment" {
  */
 resource "github_actions_environment_secret" "environment_secrets" {
   for_each        = var.environment_secrets
-  repository      = github_repository_environment.environment.repository
+  repository      = var.repo_name
   environment     = var.env_code
   secret_name     = each.key
   plaintext_value = each.value
@@ -27,7 +27,7 @@ resource "github_actions_environment_secret" "environment_secrets" {
  */
 resource "github_actions_environment_variable" "environment_variables" {
   for_each      = var.environment_variables
-  repository    = github_repository_environment.environment.repository
+  repository    = var.repo_name
   environment   = var.env_code
   variable_name = each.key
   value         = each.value
