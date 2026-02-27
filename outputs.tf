cced3d83939606e364fad4a13ea0c8ff53d33c9e
William Heyland <william.heyland@newfold.com>
2026-02-27 19:19:47 +0000

    Improve outputs.

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "repo_owner" {
-  value = var.repo_owner
+  value = split("/", local.repository.full_name)[0]
 }
 
 output "repo_name" {
-  value = var.repo_name
+  value = split("/", local.repository.full_name)[1]
 }
