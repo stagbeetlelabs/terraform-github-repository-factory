da14d9664b9808f1a4dd419e2fa19a1826a0a561
William Heyland <william.heyland@newfold.com>
2026-02-27 18:50:06 +0000

    Improve outputs.

--- modules/repository-environment/outputs.tf
+++ modules/repository-environment/outputs.tf
@@ -1,12 +1,12 @@
 // It's important to get the owner and the name from the exported values to strengthen the dependency graph
 output "repo_owner" {
-  value = split("/", data.github_repository.repository.full_name)[0]
+  value = split("/", github_repository_environment.environment.repository)[0]
 }
 
 output "repo_name" {
-  value = split("/", data.github_repository.repository.full_name)[1]
+  value = split("/", github_repository_environment.environment.repository)[1]
 }
 
 output "repo_full_name" {
-  value = data.github_repository.repository.full_name
+  value = github_repository_environment.environment.repository
 }
