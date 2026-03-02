9a7dca68101b4aa55ead61f000123bb71bba12da
William Heyland <william.heyland@newfold.com>
2026-03-02 12:18:55 +0000

    Fix repo factory.

--- github.tf
+++ github.tf
@@ -28,7 +28,7 @@ resource "github_repository" "repository" {
   }
 
   // Archive the repository when it is destroyed. A useful failsafe to prevent accidental deletion of repositories.
-  archive_on_destroy = var.archive_on_destroy
+  archive_on_destroy = false
 
   // Automatically delete branch on merge. This is a very important setting to simplify the AI agent's job.
   delete_branch_on_merge = true
--- variables.tf
+++ variables.tf
@@ -41,9 +41,3 @@ variable "secrets" {
   type        = map(string)
   default     = {}
 }
-
-variable "archive_on_destroy" {
-  description = "Archive the repository when it is destroyed"
-  type        = bool
-  default     = true
-}
